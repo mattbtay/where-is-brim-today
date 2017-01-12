ab2d5485c75985f774b93e39a4430942524055f6
Matt Taylor <mattbtay@gmail.com>
2017-01-11 21:05:19 -0600

    brim photo position is now dynamic

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,13 +10,38 @@ function getPhoto(){
 		person = document.getElementById('photoPerson');
 	// get a random 1-10
 	const randPhoto = getRandomInt(1,11);
-	const randNumb = getRandomInt(1,7);
+
+	const randNumb = getRandomInt(0,6);
+
+	const brim = [
+		{ photo: "brim1.png",
+		  position: "left: 0"
+		 },
+		 { photo: "brim2.png",
+		  position: "left: 0"
+		 },
+		 { photo: "brim3.png",
+		  position: "right: 0"
+		 },
+		 { photo: "brim4.png",
+		  position: "left: 25%"
+		 },
+		 { photo: "brim5.png",
+		  position: "right: 0"
+		 },
+		 { photo: "brim6.png",
+		  position: "right: 0"
+		 }
+	];
 
 	frame.setAttribute('style', `background-image: url('images/places/funhouse${randPhoto}.jpg')`);
 
-	person.setAttribute('src', `images/brim/brim${randNumb}.png`);	
+	person.setAttribute('src', `images/brim/${brim[randNumb].photo}`);
+
+	person.setAttribute('style', `${brim[randNumb].position}`)	
 
 }
+
 
 
 $(document).ready(function(){
